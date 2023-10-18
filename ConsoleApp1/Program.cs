9d3867d9f80ef212309edfcf626fd052d2617c03
haranycheva <mimimashkabumazka@gmail.com>
2023-10-18 21:10:01 +0300

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,44 +1,34 @@
-﻿
-using System;public class HelloWorld
+﻿using System;
+public class HelloWorld
 {
     public static void Main(string[] args)
     {
         int numb = int.Parse(Console.ReadLine());
-        int hundreds = numb / 100;
-        int dozens = numb / 10 - hundreds * 10;
-        int units = numb - (hundreds * 100 + dozens * 10);
-        if (hundreds >= dozens & hundreds >= units)
-        {
-            if (dozens >= units)
-            {
-                Console.WriteLine($"{hundreds}{dozens}{units}");
+        int hundreds = numb/100;
+        int dozens = numb/10 - hundreds*10;
+        int units = numb - (hundreds*100 + dozens*10);
+        if(hundreds >= dozens & hundreds >= units){
+            if(dozens >= units){ 
+                Console.WriteLine(hundreds*100 + dozens*10 + units);
             }
-            else
-            {
-                Console.WriteLine($"{hundreds}{units}{dozens}");
+            else{
+                Console.WriteLine(hundreds*100 + units*10 + dozens);
             }
-        }
-        else if (dozens >= units)
-        {
-            if (hundreds >= units)
-            {
-                Console.WriteLine($"{dozens}{hundreds}{units}");
+          }
+          else if(dozens>=units){
+            if(hundreds >= units){ 
+                Console.WriteLine(dozens*100 + hundreds*10 + units);
+            }else{
+                Console.WriteLine(dozens*100 + units*10 + hundreds);
             }
-            else
-            {
-                Console.WriteLine($"{dozens}{units}{hundreds}");
             }
-        }
-        else
-        {
-            if (hundreds >= dozens)
-            {
-                Console.WriteLine($"{units}{hundreds}{dozens}");
-            }
-            else
-            {
-                Console.WriteLine($"{units}{dozens}{hundreds}");
+            else{
+               if(hundreds >= dozens){ 
+                Console.WriteLine(units*100 + hundreds*10 + dozens);
+               }
+            else{
+                Console.WriteLine(units*100 + dozens*10 + hundreds);
+                }
             }
         }
     }
-}
